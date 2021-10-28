ebd4edb6f59a772a66eeb983e283ed05fa016d5b
sojicute <sojicute137@gmail.com>
2021-10-28 15:27:13 +0400

    Added comments on the primary methods

--- src/marbles/Game.java
+++ src/marbles/Game.java
@@ -6,23 +6,19 @@ package marbles;
 
 import java.util.Random;
 import java.util.Scanner;
+import static marbles.Marbles.EVEN;
+import static marbles.Marbles.ODD;
 
 /**
  *
  * @author sojicute
  */
 public class Game {
-    Scanner sc = new Scanner(System.in);
-    Random random = new Random();
-    
     private Player firstPlayer, secondPlayer, currentPlayer;
     private int currentBet;
-
-    private static final String EVEN = "even";
-    private static final String ODD = "odd";
     
-    private static final int COUNT_TO_WIN = 20;
-    private static final int MIN_BET = 1;
+    static final int COUNT_TO_WIN = 20;
+    static final int MIN_BET = 1;
     
     public int getCurrentBet() {
         return currentBet;
@@ -48,12 +44,29 @@ public class Game {
         this.secondPlayer = secondPlayer;
     }
 
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+    
+    /**
+     * Create game Player1 vs Player2
+     * 
+     * @param firstPlayer
+     * @param secondPlayer 
+     */
     public Game(Player firstPlayer, Player secondPlayer) {
         this.firstPlayer = firstPlayer;
         this.secondPlayer = secondPlayer;
     }
     
     
+    /**
+     *  Initializes the game
+     */
     public void init() {
         System.out.println("Начало игры!");
         System.out.println("Игрок №1 "+firstPlayer.getName());
@@ -61,60 +74,42 @@ public class Game {
         currentPlayer = firstPlayer;
     }
     
-    public void turnMakeBet() {
-        int playerBet;
-        
-        if ("AI".equals(currentPlayer.getPlayerType())) {
-            System.out.println(currentPlayer.getName()+" думает над ставкой");
-            
-            
-            playerBet = random.nextInt(currentPlayer.getMarbles()-MIN_BET)+ MIN_BET;
-            System.out.println(currentPlayer.getName()+" сделал ставку");
-        } else {
-            System.out.println(currentPlayer.getName()+", введите число которое хотите поставить!");
-
-            while ((playerBet = sc.nextInt()) < MIN_BET || playerBet > currentPlayer.getMarbles()) {
-                System.out.println("Неверно! Введите число от 1 до " + currentPlayer.getMarbles());
-            }
-            
-            System.out.println(currentPlayer.getName()+", вы поставили ставку в размере "+ playerBet +" шар(ов)");
-        }
-        
-        this.currentBet = playerBet;
-        
-        nextPlayer();
-    }
     
-    
-    
-
-    
-    public void turnGuessBet() {
-        String playerGuess;
-        
-        if ("AI".equals(currentPlayer.getPlayerType())) {
-            playerGuess = isEven(random.nextInt(100));
-            System.out.println(currentPlayer.getName()+" пытается угадать и говорит что число " + playerGuess);
-        } else {
-            System.out.println(currentPlayer.getName()+", угадайте четное или нечетное число, для этого введите even если четное и odd если нечетное");
-            
-            while (!((playerGuess = sc.next()).equals(EVEN) || playerGuess.equals(ODD))) {
-                System.out.println("Неверно! Введите even или odd!");
-            }
-        }
-        
-        
+    /**
+     * Exchanges marbles between players
+     * 
+     * @param playerGuess have a value even or odd
+     */
+    public void transferMarbles(String playerGuess) {
         if (isEven(currentBet).equals(playerGuess)) {
-            currentPlayer.setMarbles(currentPlayer.getMarbles() + currentBet);
-            oppositePlayer(currentPlayer).setMarbles(oppositePlayer(currentPlayer).getMarbles() - currentBet);
+
+   
+            if (oppositePlayer(currentPlayer).getMarbles() >= currentBet) {
+                currentPlayer.setMarbles(currentPlayer.getMarbles() + currentBet);
+                oppositePlayer(currentPlayer).setMarbles(oppositePlayer(currentPlayer).getMarbles() - currentBet);
+            } else {
+                currentPlayer.setMarbles(currentPlayer.getMarbles() + oppositePlayer(currentPlayer).getMarbles());
+                oppositePlayer(currentPlayer).setMarbles(0);        
+            }
             System.out.println("Верно! Игрок " + currentPlayer.getName()+ " получает "+currentBet+ " шар(ов)");
         } else {
-            currentPlayer.setMarbles(currentPlayer.getMarbles() - currentBet);
-            oppositePlayer(currentPlayer).setMarbles(oppositePlayer(currentPlayer).getMarbles()  + currentBet);
+            
+            if (currentPlayer.getMarbles() >= currentBet) {
+                currentPlayer.setMarbles(currentPlayer.getMarbles() - currentBet);
+                oppositePlayer(currentPlayer).setMarbles(oppositePlayer(currentPlayer).getMarbles()  + currentBet);
+            } else {
+                oppositePlayer(currentPlayer).setMarbles(oppositePlayer(currentPlayer).getMarbles()  + currentPlayer.getMarbles());
+                currentPlayer.setMarbles(0);
+            }
             System.out.println("Неудача! Игрок " + currentPlayer.getName()+ " теряет "+currentBet+ " шар(ов)");
         }
     }
-    
+    /**
+    * Return an opponent based on the player arg
+    * 
+    * @param player set currentPlayer
+    * @return firstPlayer or secondPlayer
+    */
     private Player oppositePlayer(Player player) {
         if(player == firstPlayer) {
             return secondPlayer;
@@ -122,7 +117,10 @@ public class Game {
         return firstPlayer ;
     }
     
-    private void nextPlayer() {
+    /**
+    * Select an opponent based on the current Player
+    */
+    public void nextPlayer() {
         if(currentPlayer == firstPlayer) {
             currentPlayer = secondPlayer;
         } else {
@@ -130,21 +128,41 @@ public class Game {
         }
     }
     
-    private String isEven(int count) {
+    
+    /**
+     * Determines the winner based on the number of balls
+     * 
+     * @return boolean
+     */
+    public boolean isWinner() {
+        if (currentPlayer.getMarbles() == COUNT_TO_WIN && oppositePlayer(currentPlayer).getMarbles() == 0 ) {
+            System.out.println("Победедитель "+ currentPlayer.getName());
+            return true;
+        } else if (oppositePlayer(currentPlayer).getMarbles() == COUNT_TO_WIN && currentPlayer.getMarbles() == 0) {
+            System.out.println("Победедитель "+ oppositePlayer(currentPlayer).getName());
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Determines the parity of a number
+     * 
+     * @param count 
+     * @return
+     */
+    public String isEven(int count) {
         if (count % 2 == 0) {
             return EVEN;
         }
         return ODD;
     }
     
-    public boolean isWin() {
-        if (currentPlayer.getMarbles() >= COUNT_TO_WIN ) {
-            System.out.println("Победедитель "+ currentPlayer.getName());
-            return true;
-        }
-        return false;
-    }
-    
+    /**
+     * Show stat players: name, count marbles
+     * 
+     * @return format string
+     */
     public String showStat() {
         return String.format(
                 "\n--------------------\n"
